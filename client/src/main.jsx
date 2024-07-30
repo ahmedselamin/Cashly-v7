1503bc4ec3d680cddc4bdbc5de76ce499fecd1c9
eddyiiz <eddyxsayf@gmail.com>
2024-07-30 17:33:27 +0200

    theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./MainLayout";
 import LandingPage from "./Pages/LandingPage";
 import HomePage from "./Pages/HomePage";
 import SettingsPage from "./Pages/SettingsPage";
 import ReportsPage from "./Pages/ReportsPage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#013e87",
+    },
+    secondary: {
+      main: "#2e74c9",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "600",
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: "600",
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: "600",
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -34,6 +60,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
