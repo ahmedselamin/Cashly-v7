935e8b2562e8cf85b6b846a85881d9029d809385
eddyiiz <eddyxsayf@gmail.com>
2024-07-30 17:17:46 +0200

    adding routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./MainLayout";
 import LandingPage from "./Pages/LandingPage";
+import HomePage from "./Pages/HomePage";
+import SettingsPage from "./Pages/SettingsPage";
+import ReportsPage from "./Pages/ReportsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "reports",
+        element: <ReportsPage />,
+      },
+      {
+        path: "settings",
+        element: <SettingsPage />,
+      },
+    ],
   },
 ]);
 
