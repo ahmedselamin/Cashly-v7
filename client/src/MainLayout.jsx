f4faacf75aebbb6d4b15680e25889d8c51ef9ac5
eddyiiz <eddyxsayf@gmail.com>
2024-07-30 17:15:19 +0200

    main lyout

---
+++ client/src/MainLayout.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Box,
+  CssBaseline,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const MainLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleSidebar}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive Sidebar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Sidebar open={sidebarOpen} onClose={toggleSidebar} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8 }}>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default MainLayout;
