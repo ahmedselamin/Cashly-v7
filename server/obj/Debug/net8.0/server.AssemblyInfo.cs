9f282f01eb8438490e706e958dcfb5ef428c77fc
eddyiiz <eddyxsayf@gmail.com>
2024-07-31 07:33:46 +0200

    essential stuff

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80bb9ae59c6eb2c1fba2e28e63fb97c78a87ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cbbdb950e07e637bd4b2628c35ed6ddb9a8038")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
